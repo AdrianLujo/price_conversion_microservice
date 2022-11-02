f1075a5d45122bcc561291e3cc408284105d5631
AdrianLujo <84686636+AdrianLujo@users.noreply.github.com>
2022-11-02 17:34:13 -0400

    Add files via upload
    Initial Commit

---
+++ currency_converter_microservice/test_price_conv_ms/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import PriceConv from './PriceConv';
+
+const App = () => (
+    <div className="App">
+      <header>Testing Price Conversion Service</header>
+      <div>
+        <PriceConv />
+      </div>
+    </div>
+  );
+
+
+export default App;
